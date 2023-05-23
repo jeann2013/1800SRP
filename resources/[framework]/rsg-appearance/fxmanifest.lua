01f58be39cd90d320e9d02b748ec312fce2a703e
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-05-22 23:26:35 -0500

    translate esp and eng

--- resources/[framework]/rsg-appearance/fxmanifest.lua
+++ resources/[framework]/rsg-appearance/fxmanifest.lua
@@ -2,6 +2,13 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
+shared_scripts {
+  '@rsg-core/shared/locale.lua',
+  'locales/en.lua',
+  'locales/*.lua',
+  "config.lua"
+}
+
 client_scripts {
     'utils/hairs.lua',
     'utils/cloth_hash_names.lua',
