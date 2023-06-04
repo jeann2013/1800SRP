1cb6df20eaaf2c444dc0fbf0c67724a97e643d3d
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-06-04 05:03:05 -0500

    add

--- resources/[external]/1800playernames/server.lua
+++ resources/[external]/1800playernames/server.lua
@@ -1,8 +1,7 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
 
-RSGCore.Functions.CreateCallback('1800:returnPlayerName', function(source,callback)
-    print(source)
+RSGCore.Functions.CreateCallback('1800:returnPlayerName', function(source,callback)    
     local Callback = callback
     local _source = source    
     local Player = RSGCore.Functions.GetPlayer(_source)    
