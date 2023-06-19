4c6feb017717144fc6df49e1d11ad7a249dc2aea
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-06-18 21:37:47 -0500

    add

--- resources/[framework]/rsg-essentials/client/commands.lua
+++ resources/[framework]/rsg-essentials/client/commands.lua
@@ -7,15 +7,3 @@ CreateThread(function ()
     Wait(1000)
     TriggerEvent('chat:addSuggestion','/hideui', 'Toggle all UIs ON/OFF (i.e. for taking Screenshot)', {})
 end)
-
-RegisterCommand('spectate', "Especteando un Jugador", { { name = "id", help = "ID del Jugador" } }, true, function(source, args)
-    local player = tonumber(args[1])
-    local Player = RSGCore.Functions.GetPlayer(player)
-
-    if Player ~= nil then
-        TriggerClientEvent('spec:client:SpectatePlayer', source, player)
-        return
-    end
-
-    RSGCore.Functions.Notify(source, "Juagdo no existe!", 'error', 3000)
-end, 'admin')
