08499ae2fb8801519429d99a7e6ac51e101acf80
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-06-21 20:27:04 -0500

    add

--- resources/[framework]/rsg-essentials/client/maxcores.lua
+++ resources/[framework]/rsg-essentials/client/maxcores.lua
@@ -147,7 +147,7 @@ end
 
 local a2 = DataView.ArrayBuffer(12* 8)
 local a3 = DataView.ArrayBuffer(12* 8)
-Citizen.InvokeNative("0xCB5D11F9508A928D", 4, a2:Buffer(), a3:Buffer(), GetHashKey("UPGRADE_HEALTH_TANK_1"), 1084182731, 5, 752097756)
+Citizen.InvokeNative("0xCB5D11F9508A928D", 2, a2:Buffer(), a3:Buffer(), GetHashKey("UPGRADE_HEALTH_TANK_1"), 1084182731, 5, 752097756)
 local a2 = DataView.ArrayBuffer(12* 8)
 local a3 = DataView.ArrayBuffer(12* 8)
 Citizen.InvokeNative("0xCB5D11F9508A928D", 4, a2:Buffer(), a3:Buffer(), GetHashKey("UPGRADE_STAMINA_TANK_1"), 1084182731, 5, 752097756)
