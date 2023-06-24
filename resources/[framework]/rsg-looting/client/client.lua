40462492752cbe71e1e929479f867d78bd646c99
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-06-24 13:56:19 -0500

    add

--- resources/[framework]/rsg-looting/client/client.lua
+++ resources/[framework]/rsg-looting/client/client.lua
@@ -2,91 +2,48 @@ local looting = false
 
 Citizen.CreateThread(function()
     while true do
-		Citizen.Wait(0)
-		Citizen.Wait(0)
+		Citizen.Wait(0)				
 		if IsControlJustPressed(0, 1101824977) and not IsPedInAnyVehicle(player, true) and not looting then
-        if IsControlJustPressed(0, 1101824977) and not IsPedInAnyVehicle(player, true) and not looting then
-			local shape = true
             local shape = true
-			while shape do
             while shape do
-				Wait(0)
                 Wait(0)
-				local player = PlayerPedId()
                 local player = PlayerPedId()
-				local coords = GetEntityCoords(player)
                 local coords = GetEntityCoords(player)
-				local entityHit = 0
                 local entityHit = 0
-				local shapeTest = StartShapeTestBox(coords.x, coords.y, coords.z, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, true, 8, player)
                 local shapeTest = StartShapeTestBox(coords.x, coords.y, coords.z, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, true, 8, player)
-				local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
                 local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
-				local type = GetPedType(entityHit)
                 local type = GetPedType(entityHit)
-				--local dead = IsEntityDead(entityHit)
                 --local dead = IsEntityDead(entityHit)
-				local PressTime = 0
                 local PressTime = 0
-				--if type == 4 and dead then -- only dead npcs
                 --if type == 4 and dead then -- only dead npcs
-				if type == 4 then
                 if type == 4 then
-					local looted = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
                     local looted = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
-					if not looted then
                     if not looted then
-						shape = false
                         shape = false
-						looting = true
                         looting = true
-						PressTime = GetGameTimer()
                         PressTime = GetGameTimer()
-						while looting do
                         while looting do
-							Wait(0)
                             Wait(0)
-							if IsControlJustReleased(0, 1101824977) then
                             if IsControlJustReleased(0, 1101824977) then
-								KeyHeldTime = GetGameTimer() - PressTime
                                 KeyHeldTime = GetGameTimer() - PressTime
-								PressTime = 0
                                 PressTime = 0
-								if KeyHeldTime > 250 then
                                 if KeyHeldTime > 250 then
-									looting = false
                                     looting = false
-									Wait(500)
                                     Wait(500)
-									local lootedcheck = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
                                     local lootedcheck = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
-									if lootedcheck then
                                     if lootedcheck then
-										TriggerServerEvent('rsg-looting:server:lootreward')
                                         TriggerServerEvent('rsg-looting:server:lootreward')
-									else
                                     else
-										looting = false
                                         looting = false
-									end
                                     end
-								else
                                 else
-									looting = false
                                     looting = false
-								end
                                 end
-							end
                             end
-						end
                         end
-					end
                     end
-				end
                 end
-			end
             end
-		end
         end
     end
 end)
