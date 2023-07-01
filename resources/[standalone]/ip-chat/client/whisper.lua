ed04e8e6fcc07a5d6ac407f49aa69538fe06b719
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-01 16:58:33 -0500

    add

--- resources/[standalone]/ip-chat/client/whisper.lua
+++ resources/[standalone]/ip-chat/client/whisper.lua
@@ -15,7 +15,7 @@ AddEventHandler('chat:whisper', function(id, name, message, time)
         -- Check the distance and broadcast the message if it's within the bisikDistance limit
         if id2 == id1 or GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(id1)), GetEntityCoords(GetPlayerPed(id2)), true) < Config.bisikDistance then
             TriggerEvent('chat:addMessage', {
-                template = '<div class="chat-message ooc"><i class="fas fa-comment"></i> <b><span style="color: #0b0d0b">[WHISPER] {0}</span>&nbsp;<span style="font-size: 14px; color: #0b0d0b;">{2}</span></b><div style="margin-top: 5px; font-weight: 300;">{1}</div></div>',
+                template = '<div class="chat-message ooc"><i class="fas fa-comment"></i> <b><span style="color: #0b0d0b">[MP] {0}</span>&nbsp;<span style="font-size: 14px; color: #0b0d0b;">{2}</span></b><div style="margin-top: 5px; font-weight: 300;">{1}</div></div>',
                 args = {playerName, message, time}
             })
         end
