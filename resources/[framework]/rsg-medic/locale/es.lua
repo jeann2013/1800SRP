7a82c5a070f48a637d81519f27459509e3f10474
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-05 18:41:02 -0500

    add

--- resources/[framework]/rsg-medic/locale/es.lua
+++ resources/[framework]/rsg-medic/locale/es.lua
@@ -8,6 +8,7 @@ local Translations = {
     },
     success = {
         revived = 'Reviviste a una persona',
+        medic_has_been_called = '¡El médico(a) ha sido llamada!',
     },
     info = {
         revive_player_a = 'Revive a un jugador o a ti mismo (solo administrador)',
@@ -16,6 +17,30 @@ local Translations = {
         blip_text = 'Alerta médica - %{text}',
         new_call = 'Nueva llamada',
     },
+    menu = {
+        open_menu = 'Open ',
+        toggle_duty_menu = "Toggle Duty",
+        medical_supplies_menu = "Medical Supplies",
+        medic_storage_menu = "Medic Storage",
+        job_management = "Job Management",
+        close_menu_menu = "Close Menu",
+    },
+    text = {
+        respawn_in_text = 'REPARECER EN ',
+        seconds_text = ' SEGUNDOS..',
+        press_e_to_respawn = 'PULSA [E] PARA REAPARAR',
+        press_e_ro_respwn_press_j_call = 'PULSE [E] PARA RESPETAR - PRESIONE [J] PARA SOLICITAR ASISTENCIA',
+        press_j_to_call_assis = 'PRESIONE [J] PARA SOLICITAR ASISTENCIA',
+        a_person_needs_medical_help = '¡Una persona necesita ayuda médica!',
+    },
+    progressbar = {
+        reviving_progressbar = "Reviviendo...",
+        treating_wounds = "Tratamiento de heridas...",
+    },
+    primary = {
+        you_lost_all_your_possessions = '¡Perdiste todas tus posesiones!',
+        you_lost_all_your_cash = '¡Perdiste todo tu dinero!',
+    }
 }
 
 if GetConvar('rsg_locale', 'en') == 'es' then
