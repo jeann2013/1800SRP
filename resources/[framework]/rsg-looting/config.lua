2c1b2ca6c4b458a902c6644c8c7ffc0e889263a0
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-09 16:15:46 -0500

    add

--- resources/[framework]/rsg-looting/config.lua
+++ resources/[framework]/rsg-looting/config.lua
@@ -2,26 +2,16 @@ Config = {}
 
 -- list of common items / add more as required (must be in your shared inventory items)
 Config.CommonItems = {
-    "bread", -- example
-	"water", -- example
-    "corn",
-    "ammo_pistol",
-    "ammo_revolver",
-    "weapon_rifle_springfield",
-    "weapon_kit_binoculars",
-    "weapon_lasso",
-    "weapon_pistol_m1899",
-    "weapon_revolver_navy"
+    "bread", 
+	"water", 
+    "pocket_watch",
+    "pocket_watch_gold",
+    "pocket_watch_silver",
+    "pocket_watch_platinum",
 }
 
 -- list of rare items / add more as required (must be in your shared inventory items)
-Config.RareItems = {   
-    "cigar",
-    "perfect_bear_pelt",
-    "fertilizer",
-    "goldbar",
-    "wood",
-    "weapon_revolver_cattleman",
-    "weapon_bow_improved",
-    "weapon_thrown_dynamite"
+Config.RareItems = {      
+    "weapon_revolver_cattleman",    
+    "weapon_pistol_m1899",
 }
