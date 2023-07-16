bda17abfd1c4005ea710d2d93b580267e5da12e2
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-16 13:02:31 -0500

    add

--- resources/[framework]/rsg-essentials/client/smallfixes.lua
+++ resources/[framework]/rsg-essentials/client/smallfixes.lua
@@ -51,25 +51,25 @@ Citizen.CreateThread(function()
 end)
 
 -- pause menu animation thanks to ManLikeTJB#4518
-Citizen.CreateThread(function()
-    while true do
-        Wait(0)
-        local ped = PlayerPedId()
-        if IsPauseMenuActive() and not PauseOpen then
-            SetCurrentPedWeapon(ped, 0xA2719263, true) -- set unarmed
-            SetCurrentPedWeapon(ped, GetHashKey("weapon_unarmed"))
-            if not IsPedOnMount(ped) then
-                TaskStartScenarioInPlace(PlayerPedId(), GetHashKey("WORLD_HUMAN_SIT_GROUND_READING_BOOK"), -1, true, "StartScenario", 0, false)
-            end
-            PauseOpen = true
-        end
+-- Citizen.CreateThread(function()
+--     while true do
+--         Wait(0)
+--         local ped = PlayerPedId()
+--         if IsPauseMenuActive() and not PauseOpen then
+--             SetCurrentPedWeapon(ped, 0xA2719263, true) -- set unarmed
+--             SetCurrentPedWeapon(ped, GetHashKey("weapon_unarmed"))
+--             if not IsPedOnMount(ped) then
+--                 TaskStartScenarioInPlace(PlayerPedId(), GetHashKey("WORLD_HUMAN_SIT_GROUND_READING_BOOK"), -1, true, "StartScenario", 0, false)
+--             end
+--             PauseOpen = true
+--         end
 
-        if not IsPauseMenuActive() and PauseOpen then
-            ClearPedTasks(ped)
-            Wait(4000)
-            SetCurrentPedWeapon(ped, 0xA2719263, true) -- set unarmed
-            SetCurrentPedWeapon(ped, GetHashKey("weapon_unarmed"))
-            PauseOpen = false
-        end
-    end
-end)
+--         if not IsPauseMenuActive() and PauseOpen then
+--             ClearPedTasks(ped)
+--             Wait(4000)
+--             SetCurrentPedWeapon(ped, 0xA2719263, true) -- set unarmed
+--             SetCurrentPedWeapon(ped, GetHashKey("weapon_unarmed"))
+--             PauseOpen = false
+--         end
+--     end
+-- end)
