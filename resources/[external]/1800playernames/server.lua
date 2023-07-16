dde0a89c8e6e365d7cc166d4e59bbf3e021ddb4a
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-16 15:15:34 -0500

    add

--- resources/[external]/1800playernames/server.lua
+++ resources/[external]/1800playernames/server.lua
@@ -7,21 +7,24 @@ RegisterNetEvent('1800:returnPlayerName', function()
     TriggerClientEvent('1800:client:showNames', src, players)
 end)
 
-CreateThread(function()
+CreateThread(function(source)
+    local src = source    
     while true do
         local tempPlayers = {}
         for _, v in pairs(RSGCore.Functions.GetPlayers()) do
             local targetped = GetPlayerPed(v)
-            local ped = RSGCore.Functions.GetPlayer(v)			
-            tempPlayers[#tempPlayers + 1] = {
-                name = v .. '-' .. (ped.PlayerData.charinfo.firstname or '') .. ' ' .. (ped.PlayerData.charinfo.lastname or ''),
-                id = v,
-                coords = GetEntityCoords(targetped),
-                cid = ped.PlayerData.charinfo.firstname .. ' ' .. ped.PlayerData.charinfo.lastname,
-                citizenid = ped.PlayerData.citizenid,
-                sources = GetPlayerPed(ped.PlayerData.source),
-                sourceplayer = ped.PlayerData.source
-            }
+            local ped = RSGCore.Functions.GetPlayer(v)		
+            if src ~= v then	
+                tempPlayers[#tempPlayers + 1] = {
+                    name = v .. '-' .. (ped.PlayerData.charinfo.firstname or '') .. ' ' .. (ped.PlayerData.charinfo.lastname or ''),
+                    id = v,
+                    coords = GetEntityCoords(targetped),
+                    cid = ped.PlayerData.charinfo.firstname .. ' ' .. ped.PlayerData.charinfo.lastname,
+                    citizenid = ped.PlayerData.citizenid,
+                    sources = GetPlayerPed(ped.PlayerData.source),
+                    sourceplayer = ped.PlayerData.source
+                }
+            end
         end        
         table.sort(tempPlayers, function(a, b)
             return a.id < b.id
