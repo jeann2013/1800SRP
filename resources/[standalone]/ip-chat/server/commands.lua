21015bfa35a9a55d7d9656d6446ea7707c4f14e1
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-15 20:29:57 -0500

    add

--- resources/[standalone]/ip-chat/server/commands.lua
+++ resources/[standalone]/ip-chat/server/commands.lua
@@ -257,7 +257,7 @@ RegisterCommand('ooc', function(source, args, rawCommand)
     local lastname = PlayerData.charinfo.lastname
     local playerName = firstname .. ' ' .. lastname
     TriggerClientEvent('chat:addMessage', -1, {
-        template = '<div class="chat-message ooc"> <b><span style="color: #ffc107">[OOC] {0}</span></b>{2}</div>',
+        template = '<div class="chat-message ooc"> <b><span style="color: #ffc107">/ooc * {0}</span></b> {2}</div>',
         args = {playerName, time, message}
     })
     TriggerEvent('rsg-log:server:CreateLog', 'ooc', 'OOC', 'white', '**' .. GetPlayerName(source) .. '** (CitizenID: ' .. Player.PlayerData.citizenid .. ' | ID: ' .. source .. ') **Message:** ' .. message, false)
