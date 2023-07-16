935576da0f52e3d51687135395f1722dc5192bde
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-16 09:33:37 -0500

    add

--- resources/[framework]/rsg-essentials/client/hudcomponents.lua
+++ resources/[framework]/rsg-essentials/client/hudcomponents.lua
@@ -26,8 +26,8 @@ Citizen.CreateThread(function()
     -- Citizen.InvokeNative(0xC116E6DF68DCE667, 0, 2) -- ICON_STAMINA / HIDE
     -- Citizen.InvokeNative(0xC116E6DF68DCE667, 1, 2) -- ICON_STAMINA_CORE / HIDE
     
-    Citizen.InvokeNative(0xC116E6DF68DCE667, 2, 2) -- ICON_DEADEYE / HIDE
-    Citizen.InvokeNative(0xC116E6DF68DCE667, 3, 2) -- ICON_DEADEYE_CORE / HIDE
+    -- Citizen.InvokeNative(0xC116E6DF68DCE667, 2, 2) -- ICON_DEADEYE / HIDE
+    -- Citizen.InvokeNative(0xC116E6DF68DCE667, 3, 2) -- ICON_DEADEYE_CORE / HIDE
     
     -- Citizen.InvokeNative(0xC116E6DF68DCE667, 4, 2) -- ICON_HEALTH / HIDE
     -- Citizen.InvokeNative(0xC116E6DF68DCE667, 5, 2) -- ICON_HEALTH_CORE / HIDE
