4cc552e79c4e0b2bbcd93d714f9dfdff2ef3429d
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-07-22 16:11:36 -0500

    add

---
+++ resources/[standalone]/ip-chat/client/me.lua
@@ -0,0 +1,27 @@
+local RSGcore = exports['rsg-core']:GetCoreObject()
+
+RegisterNetEvent('chat:me')
+AddEventHandler('chat:me', function(id, name, message, time)
+    local id1 = PlayerId()
+    local id2 = GetPlayerFromServerId(id)
+    local sourcePlayer = GetPlayerFromServerId(id1) 
+
+    -- Asynchronously retrieve the player data
+    RSGcore.Functions.GetPlayerData(function(PlayerData)
+        local firstname = PlayerData.charinfo.firstname
+        local lastname = PlayerData.charinfo.lastname
+        local playerName = firstname .. ' ' .. lastname
+
+        -- Check the distance and broadcast the message if it's within the bisikDistance limit
+        if id2 == id1 or GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(id1)), GetEntityCoords(GetPlayerPed(id2)), true) < Config.WhisperDistance then
+            TriggerEvent('chat:addMessage', {
+                template = '<div class="chat-message ooc"> <b><span style="color: #0b0d0b"> * {0}</span>&nbsp;<span style="font-size: 14px; color: #0b0d0b;">{2}</span></b><div style="margin-top: 5px; font-weight: 300;">{1}</div></div>',
+                args = {playerName, message, time}
+            })
+        end
+    end)
+end)
+
+RegisterNetEvent('ip-chat:client:SendReport', function(name, src, msg)
+    TriggerServerEvent('ip-chat:server:SendReport', name, src, msg)
+end)
